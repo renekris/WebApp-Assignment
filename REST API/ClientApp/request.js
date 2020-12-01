afe4ff8c531996260b0c1ccfbafd6bd8162b73d0
Renekris <renekrispohlak@gmail.com>
2020-12-01 19:58:39 +0200

    Cleanup

--- REST API/ClientApp/request.js
+++
@@ -1,7 +0,0 @@
-angular.module('demo', [])
-  .controller('Hello', function ($scope, $http) {
-    $http.get('https://localhost:5001/api/BagWithParcels').
-      then(function (response) {
-        $scope.greeting = response.data;
-      });
-  });
